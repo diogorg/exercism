90c1d29f5f408dd879b383ab81f3aee9283df94c
Diogo Gutierre <diogorg@gmail.com>
2023-12-04 17:45:18 -0300

    fix: diamond exercise

--- typescript/diamond/diamond.ts
+++ typescript/diamond/diamond.ts
@@ -1,5 +1,6 @@
 export function makeDiamond(letter: string): string {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const space = ' ';
 
     if (!alphabet.includes(letter)) {
         throw new Error('Input must be a letter from A to Z');
@@ -9,18 +10,18 @@ export function makeDiamond(letter: string): string {
     const index = alphabet.indexOf(letter.toUpperCase());
 
     for (let i = 0; i <= index; i++) {
-        const spaces = ' '.repeat(index - i);
+        const spaces = space.repeat(index - i);
         const repeat = 2 * i - 1 >= 0 ? 2 * i - 1 : 0;
-        const row = spaces + alphabet[i] + ' '.repeat(repeat) + (i > 0 ? alphabet[i] : '') + spaces;
+        const row = spaces + alphabet[i] + space.repeat(repeat) + (i > 0 ? alphabet[i] : '') + spaces;
         diamond.push(row);
     }
 
     for (let i = index - 1; i >= 0; i--) {
-        const spaces = ' '.repeat(index - i);
+        const spaces = space.repeat(index - i);
         const repeat = 2 * i - 1 >= 0 ? 2 * i - 1 : 0;
-        const row = spaces + alphabet[i] + ' '.repeat(repeat) + (i > 0 ? alphabet[i] : '') + spaces;
+        const row = spaces + alphabet[i] + space.repeat(repeat) + (i > 0 ? alphabet[i] : '') + spaces;
         diamond.push(row);
     }
 
-    return diamond.join('\n');
+    return diamond.join('\n') + '\n';
 }
